ff3f9c60c301af9de3f3004d80e7aa48174338b2
Benncee <ianescobar96@gmail.com>
2023-05-18 20:12:21 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/login.component'
 import SignUp from './components/signup.component'
+import Navbar from './components/navbarcomponent'
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
-              MyCodeHub
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+        <Navbar />
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
@@ -42,4 +23,5 @@ function App() {
     </Router>
   )
 }
+
 export default App
--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,46 +1,61 @@
-import React, { Component } from 'react'
-export default class Login extends Component {
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <form>
+      <div>
         <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Email address</label>
             <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={this.state.email}
+              onChange={this.onChange}
             />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
           </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Login
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              placeholder="Enter password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">
+            Submit
           </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
-    )
+          <p className="forgot-password text-right">
+            <Link to="/forgot-password">Forgot password?</Link>
+          </p>
+        </form>
+      </div>
+    );
   }
 }
+
+export default Login;
